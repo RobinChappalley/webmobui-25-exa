44b62e72fcdf00caf396cf8cf794b301b5d05939
RobinChappalley <robinchappalley@gmail.com>
2025-03-26 09:08:42 +0100

    Add number of kitchen's genre

--- src/sections/genres.js
+++ src/sections/genres.js
@@ -3,11 +3,12 @@ import { loadGenres } from "../api";
 import "../elements/genre-item";
 
 const genreList = document.querySelector(".genres");
+const listTitle = document.querySelector("#section-genres h4");
 
 export const displayGenreList = async () => {
   genreList.innerHTML = "";
-
   const genres = await loadGenres();
+  listTitle.innerHTML = `Genres de recettes (${genres.length})`;
 
   genres.forEach((genre) => {
     const genreItem = document.createElement("genre-item");
