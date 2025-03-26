6f1a2176e49412c909da5f6a303bae0fed8edf43
RobinChappalley <robinchappalley@gmail.com>
2025-03-26 08:43:16 +0100

    Add files to track genres

--- src/api.js
+++ src/api.js
@@ -1,2 +1,12 @@
 // URL de base du serveur
 const BASE_URL = 'https://webmobui-25-exa-backend-dbb26fcb95b3.herokuapp.com'
+
+const loadJson = (url) => fetch(url).then((response) => response.json())
+
+const loadGenres = () => loadJson(`${BASE_URL}/api/genres`)
+
+// const loadSongs = (id) => loadJson(`${BASE_URL}/api/artists/${id}/songs`)
+
+// const loadSearch = (query) => loadJson(`${BASE_URL}/api/songs/search/${query}`)
+
+export { loadGenres }
