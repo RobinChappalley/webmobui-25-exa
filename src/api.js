b416a582d6f53261217539cbcdda0d902b62c7ec
RobinChappalley <robinchappalley@gmail.com>
2025-03-26 09:22:40 +0100

    Add new api query to get the receipes link to a genre

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 // URL de base du serveur
-const BASE_URL = 'https://webmobui-25-exa-backend-dbb26fcb95b3.herokuapp.com'
+const BASE_URL = "https://webmobui-25-exa-backend-dbb26fcb95b3.herokuapp.com";
 
-const loadJson = (url) => fetch(url).then((response) => response.json())
+const loadJson = (url) => fetch(url).then((response) => response.json());
 
-const loadGenres = () => loadJson(`${BASE_URL}/api/genres`)
+const loadGenres = () => loadJson(`${BASE_URL}/api/genres`);
+
+const loadRecipes = (id) => loadJson(`${BASE_URL}/api/genres/${id}/recipes`);
 
 // const loadSongs = (id) => loadJson(`${BASE_URL}/api/artists/${id}/songs`)
 
 // const loadSearch = (query) => loadJson(`${BASE_URL}/api/songs/search/${query}`)
 
-export { loadGenres }
+export { loadGenres, loadRecipes };
